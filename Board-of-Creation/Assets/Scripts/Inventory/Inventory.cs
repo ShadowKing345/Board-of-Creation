14e4eec86a8fbc37a87cd24ea65774cc16abb782
Shadowking345 <alexandruturcanu915@gmail.com>
2020-05-15 11:16:22 +0300

    Initial Commit.

---
+++ Board-of-Creation/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Objects;
+using UnityEngine;
+
+namespace Inventory
+{
+    [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/Inventory", order = 0)]
+    public class Inventory : ScriptableObject
+    {
+        [SerializeField] private List<Itemstack> slots;
+
+        public Itemstack GetItemStackFromSlot(int slot)
+        {
+            return slots[slot];
+
+        }
+
+        // todo: implements binary search.
+        public int ContainsItemstack(Itemstack itemstack)
+        {
+            for (var index = 0; index < slots.Count; index++)
+            {
+                var slot = slots[index];
+                if (slot.IsEmpty()) continue;
+
+                if (slot.item == itemstack.item)
+                    return index;
+            }
+
+            return -1;
+        }
+
+        public Itemstack AddItemStack(Itemstack stack)
+        {
+            var index = ContainsItemstack(stack);
+            if (index != -1)
+            {
+                return slots[index].Add(stack.Amount);
+            }
+            else
+            {
+                for (int i = 0; i < slots.Count; i++)
+                {
+                    if (slots[i].IsEmpty())
+                    {
+                        slots[i] = stack;
+                        return new Itemstack();
+                    }
+
+                }
+            }
+
+            return null;
+        }
+
+        public Itemstack RemoveItemStackFromSlot(int index)
+        {
+            if (slots[index].IsEmpty()) return new Itemstack();
+
+            var result = new Itemstack(slots[index]);
+            return result;
+        }
+        
+        // todo: implements a sort.
+        public void Sort()
+        {
+            
+        }
+    }
+}
---
+++ Board-of-Creation/Assets/Scripts/Objects/Item.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+namespace Objects
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        public Sprite image;
+        [TextArea(10, 20)] public string description;
+    }
+
+    [System.Serializable]
+    public class Itemstack
+    {
+        public Item item;
+        [SerializeField]
+        private int amount;
+        public int Amount
+        {
+            get => amount;
+            set => amount = Math.Abs(value);
+        }
+        public int stackSize = 999;
+        
+        public Itemstack(){}
+
+        public Itemstack(Item item, int amount, int stackSize)
+        {
+            this.item = item;
+            this.amount = amount;
+            this.stackSize = stackSize;
+        }
+
+        public Itemstack(Itemstack itemstack)
+        {
+            item = itemstack.item;
+            amount = itemstack.amount;
+            stackSize = itemstack.stackSize;
+        }
+
+        public Itemstack Add(int addBy)
+        {
+            var remainder = new Itemstack {item = item};
+
+            amount += addBy;
+            if (amount <= stackSize) return remainder;
+
+            remainder.amount = amount - stackSize;
+            amount = stackSize;
+
+            return remainder;
+        }
+
+        public Itemstack Subtract(int subtractBy)
+        {
+            var remainder = new Itemstack {item = item, amount = subtractBy};
+
+            amount -= subtractBy;
+
+            if (amount >= 0) return remainder;
+
+            remainder.amount += amount;
+            amount = 0;
+
+            return remainder;
+        }
+
+        public bool IsEmpty()
+        {
+            return amount <= 0 || item == null;
+        }
+    }
+}
