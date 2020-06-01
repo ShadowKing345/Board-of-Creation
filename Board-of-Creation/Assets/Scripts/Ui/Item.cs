567658e8b1c58b5a3ac3074d3d40a2b534b3f312
Alex <alexandruturcanu915@gmail.com>
2020-06-01 16:16:11 +0300

    Started on the Ui inventory and added Drag and Drop support for items.

---
+++ Board-of-Creation/Assets/Scripts/Ui/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Objects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class Item : MonoBehaviour
+    {
+        [SerializeField] private Text text;
+        public Itemstack stack = null;
+        private Image _image;
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+        }
+
+        private void Update()
+        {
+            if(stack == null) return;
+            
+            _image.sprite = stack.item.image;
+            text.text = stack.Amount.ToString();
+        }
+    }
+}
