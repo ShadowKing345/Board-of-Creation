cc3966e1225a6d12d9006cbd0bc4ae9ba80e2be5
Alex <alexandruturcanu915@gmail.com>
2020-06-08 11:03:40 +0300

    Separated ItemInventory and ItemStackInventory into two separate classes.

--- Board-of-Creation/Assets/Scripts/Inventory/Inventory.cs
+++ Board-of-Creation/Assets/Scripts/Inventory/Inventory.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Objects;
+﻿using Objects;
 using UnityEngine;
 
 namespace Inventory
@@ -8,63 +7,5 @@ namespace Inventory
     public class Inventory : ScriptableObject
     {
         public Itemstack[] slots;
-
-        public Itemstack GetItemStackFromSlot(int slot)
-        {
-            return slots[slot];
-
-        }
-
-        // todo: implements binary search.
-        public int ContainsItemstack(Itemstack itemstack)
-        {
-            for (var index = 0; index < slots.Length; index++)
-            {
-                var slot = slots[index];
-                if (slot.IsEmpty()) continue;
-
-                if (slot.item == itemstack.item)
-                    return index;
-            }
-
-            return -1;
-        }
-
-        public Itemstack AddItemStack(Itemstack stack)
-        {
-            var index = ContainsItemstack(stack);
-            if (index != -1)
-            {
-                return slots[index].Add(stack.Amount);
-            }
-            else
-            {
-                for (int i = 0; i < slots.Length; i++)
-                {
-                    if (slots[i].IsEmpty())
-                    {
-                        slots[i] = stack;
-                        return new Itemstack();
-                    }
-
-                }
-            }
-
-            return null;
-        }
-
-        public Itemstack RemoveItemStackFromSlot(int index)
-        {
-            if (slots[index].IsEmpty()) return new Itemstack();
-
-            var result = new Itemstack(slots[index]);
-            return result;
-        }
-        
-        // todo: implements a sort.
-        public void Sort()
-        {
-            
-        }
     }
 }
---
+++ Board-of-Creation/Assets/Scripts/Inventory/ItemInventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Objects;
+using UnityEngine;
+
+namespace Inventory
+{
+    [CreateAssetMenu(fileName = "New Item Inventory", menuName = "Inventory/Item Inventory", order = 1)]
+    public class ItemInventory : ScriptableObject
+    {
+        public Item[] slots;
+
+        public Item AddItem(Item item)
+        {
+            if (slots.Contains(item)) return item;
+
+            for (var i = 0; i < slots.Length; i++)
+            {
+                if (slots[i] != null) continue;
+                slots[i] = item;
+                GameEvents.Current.UpdateItemInventory();
+                return null;
+            }
+
+            return item;
+        }
+    }
+}
