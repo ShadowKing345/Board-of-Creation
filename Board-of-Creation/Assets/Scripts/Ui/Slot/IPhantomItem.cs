381dadc5fcb346fdb985b9ca9db3c6c14f7bf9d4
Alex <alexandruturcanu915@gmail.com>
2020-06-28 12:02:08 +0300

    Worked on the PhantomItem object. Now is mostly working.

---
+++ Board-of-Creation/Assets/Scripts/Ui/Slot/IPhantomItem.cs
@@ -0,0 +1,10 @@
+﻿using Objects;
+
+namespace BoardOfCreation.Ui.Slot
+{
+    public interface IPhantomItem
+    {
+        void SetItem(Item item);
+        void SetItemStack(Itemstack itemstack);
+    }
+}
---
+++ Board-of-Creation/Assets/Scripts/Ui/Slot/PhantomItemStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Objects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BoardOfCreation.Ui.Slot
+{
+    public class PhantomItemStack : MonoBehaviour, IPhantomItem
+    {
+        [SerializeField] private Text text;
+        [SerializeField] private Itemstack stack;
+        [SerializeField] private Image image;
+
+        private void Awake()
+        {
+            if (stack == null)
+                stack = new Itemstack();
+
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            if(!image || stack.item == null) return;
+            image.sprite = stack.item.image;
+            
+            if (!text) return;
+            text.enabled = stack.Amount > 0;
+            text.text = stack.Amount.ToString();
+        }
+
+        public void SetItem(Item item)
+        {
+            stack.item = item;
+            UpdateImage();
+        }
+
+        public void SetItemStack(Itemstack itemstack)
+        {
+            stack = itemstack;
+            UpdateImage();
+        }
+
+        public void Update()
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+}
