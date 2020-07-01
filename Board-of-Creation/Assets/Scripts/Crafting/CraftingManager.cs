f37fad789dfbf4770221e6e05f04c90a00eeafec
Alex <alexandruturcanu915@gmail.com>
2020-07-01 18:39:32 +0300

    Reworked the CraftingManager to actually be the crafting manager instead of the Container for the Crafting Manager. Altered the functions to work around the modifications to the Slot system. Added MoveItemStack function for the crafting slots.

--- Board-of-Creation/Assets/Scripts/Crafting/CraftingManager.cs
+++ Board-of-Creation/Assets/Scripts/Crafting/CraftingManager.cs
@@ -1,57 +1,22 @@
-﻿using System.Collections.Generic;
-using BoardOfCreation.Ui.Slot.ItemSlot;
-using Inventory;
-using Objects;
-using Ui;
+﻿using System;
+using BoardOfCreation.Crafting.Recipe;
+using BoardOfCreation.Objects;
 using UnityEngine;
 
-namespace Crafting
+namespace BoardOfCreation.Crafting
 {
     public class CraftingManager : MonoBehaviour
     {
-        [SerializeField] private CraftingSlot input1;
-        [SerializeField] private CraftingSlot input2;
-        [SerializeField] private ResultSlot resultSlot;
-        
-        [SerializeField] private ItemInventory itemInventory;
-        [SerializeField] private List<GameObject> slots = new List<GameObject>();
-        [SerializeField] private GameObject contentView;
-        [SerializeField] private GameObject slotsPrefab;
-
-        private void Start()
+        public static CraftingManager current;
+        public void Awake()
         {
-            GameEvents.Current.OnCratingItemSlotChange += OnCratingIngredientsChange;
-            GameEvents.Current.OnRecipeResultSlotChange += item =>
-            {
-                itemInventory.AddItem(item);
-                input1.SetItem(null);
-                input2.SetItem(null);
-                resultSlot.SetItem(null);
-            };
-            GameEvents.Current.OnItemInventoryChangeEvent += UpdateItemList;
-
-            foreach (var item in itemInventory.slots)
-            {
-                var slot = Instantiate(slotsPrefab, contentView.transform);
-                slot.GetComponent<ItemSlot>().SetItem(item);
-                slots.Add(slot);
-            }
+            current = this;
         }
 
-        private void OnCratingIngredientsChange(Item item)
+        public Item FindRecipeResult(Item input1, Item input2)
         {
-            var recipe = RecipeManager.Instance.FindRecipe(input1.GetItem(), input2.GetItem());
-            resultSlot.SetItem(recipe ? recipe.result : null);
-        }
-
-        private void UpdateItemList()
-        {
-            for (var i = 0; i < itemInventory.slots.Length; i++)
-            {
-                if (!itemInventory.slots[i]) continue;
-                ItemSlot slot = slots[i].GetComponent<ItemSlot>();
-                slot.SetItem(itemInventory.slots[i]);
-            }
+            var recipe = RecipeManager.current.FindRecipe(input1, input2);
+            return !recipe ? null : recipe.result;
         }
     }
 }
---
+++ Board-of-Creation/Assets/Scripts/Ui/Containers/CraftingContainer.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Linq;
+using BoardOfCreation.Crafting;
+using BoardOfCreation.Inventory;
+using BoardOfCreation.Objects;
+using BoardOfCreation.Ui.Slot;
+using BoardOfCreation.Ui.Slot.ItemSlot;
+using UnityEngine;
+
+namespace BoardOfCreation.Ui.Containers
+{
+    public class CraftingContainer : Container
+    {
+        [SerializeField] private CraftingSlot input1;
+        [SerializeField] private CraftingSlot input2;
+        [SerializeField] private ResultSlot resultSlot;
+        
+        [SerializeField] private ItemInventory itemInventory;
+        [SerializeField] private GameObject contentView;
+
+        private void Start()
+        {
+            Clear();
+            
+            EventManager.current.OnSlotUpdated += slot =>
+            {
+                if (new ISlot[] {input1, input2}.Contains(slot))
+                    UpdateResult();
+            };
+            EventManager.current.OnResultSlotUpdated += slot =>
+            {
+                if (slot != resultSlot) return;
+                //AddItemToList(slot.Item);
+                ClearCraftingSlots();
+            };
+
+            AddSlot(input1);
+            AddSlot(input2);
+            AddSlot(resultSlot);
+            
+            ClearCraftingSlots();
+
+            StartCoroutine(InventoryCreation());
+        }
+
+        private void AddItemToList(Item item)
+        {
+            itemInventory.AddItem(item);
+            UpdateItemInventory();
+        }
+        
+        private void ClearCraftingSlots()
+        {
+            input1.ClearSlot();
+            input2.ClearSlot();
+            resultSlot.ClearSlot();
+        }
+
+        public override void MoveItemStack(int fromId, int toId)
+        {
+            if (new ISlot[] {input1, input2}.Contains(slots[toId]))
+            {
+                ISlot from = slots[fromId], to = slots[toId];
+                to.Stack = from.Stack;
+            }
+        }
+
+        private void UpdateResult()
+        {
+            Item result = CraftingManager.current.FindRecipeResult(input1.Item, input2.Item);
+            if (result)
+                resultSlot.Item = result;
+        }
+
+        private void UpdateItemInventory()
+        {
+            for (int i = 0; i < itemInventory.slots.Length; i++)
+            {
+                slots[i + 3].Item = itemInventory.slots[i];
+            }
+        }
+
+        private IEnumerator InventoryCreation()
+        {
+            var wait = new WaitForFixedUpdate();
+            foreach (var item in itemInventory.slots)
+            {
+                var slot = Instantiate(slotsPrefab, contentView.transform);
+                yield return wait;
+                AddSlot(slot.GetComponent<ISlot>(), new ItemStack() {item = item});
+            }
+        }
+    }
+}
