c7d5871d911b1b4674c924fe3ece14d785fdf8b6
Alex <alexandruturcanu915@gmail.com>
2020-07-01 18:42:58 +0300

    Moved ItemStack.cs to its own class.

---
+++ Board-of-Creation/Assets/Scripts/Objects/ItemStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace BoardOfCreation.Objects
+{
+    [Serializable]
+    public class ItemStack
+    {
+        public Item item;
+        [SerializeField]
+        private int amount;
+        public int Amount
+        {
+            get => amount;
+            set => amount = Math.Abs(value);
+        }
+        public int stackSize = 999;
+        public bool HasNoItem() => !item;
+        public bool IsEmpty() => amount <= 0 || HasNoItem();
+        
+        public ItemStack(){}
+
+        public ItemStack(Item item, int amount, int stackSize)
+        {
+            this.item = item;
+            this.amount = amount;
+            this.stackSize = stackSize;
+        }
+
+        public ItemStack(ItemStack itemStack)
+        {
+            item = itemStack.item;
+            amount = itemStack.amount;
+            stackSize = itemStack.stackSize;
+        }
+
+        public ItemStack Add(int addBy)
+        {
+            var remainder = new ItemStack {item = item};
+
+            amount += addBy;
+            if (amount <= stackSize) return remainder;
+
+            remainder.amount = amount - stackSize;
+            amount = stackSize;
+
+            return remainder;
+        }
+
+        public ItemStack Subtract(int subtractBy)
+        {
+            var remainder = new ItemStack {item = item, amount = subtractBy};
+
+            amount -= subtractBy;
+
+            if (amount >= 0) return remainder;
+
+            remainder.amount += amount;
+            amount = 0;
+
+            return remainder;
+        }
+    }
+}
