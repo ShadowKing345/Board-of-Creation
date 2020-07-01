061021d00492fff68c313ec7c981d57e1bbfe20e
Alex <alexandruturcanu915@gmail.com>
2020-07-01 18:42:26 +0300

    Created an interface for the Slots ( ISlot.cs ) to allow for easier object handling in the future.

---
+++ Board-of-Creation/Assets/Scripts/Ui/Slot/ISlot.cs
@@ -0,0 +1,28 @@
+﻿using BoardOfCreation.Objects;
+using BoardOfCreation.Ui.Containers;
+using UnityEngine.EventSystems;
+
+namespace BoardOfCreation.Ui.Slot
+{
+    public interface ISlot: IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler, IPointerClickHandler
+    {
+        Container Container { get; set; }
+        int Id { get; set; }
+
+        void UpdateSlot();
+
+        Item Item
+        {
+            get;
+            set;
+        }
+
+        ItemStack Stack
+        {
+            get;
+            set;
+        }
+
+        void ClearSlot();
+    }
+}
