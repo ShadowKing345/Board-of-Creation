653e8637ceaf05c41f2753739dcf7beb48786e06
Alex <alexandruturcanu915@gmail.com>
2020-07-01 18:45:43 +0300

    Changed the name to make more sense. Reworked the events to be more simplistic.

---
+++ Board-of-Creation/Assets/Scripts/EventManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BoardOfCreation.Objects;
+using BoardOfCreation.Ui.Slot;
+using BoardOfCreation.Ui.Slot.ItemSlot;
+using UnityEngine;
+
+namespace BoardOfCreation
+{
+    public sealed class EventManager : MonoBehaviour
+    {
+        public static EventManager current;
+        private void Awake()
+        {
+            current = this;
+        }
+
+        public event Action<ISlot> OnSlotUpdated;
+
+        public void SlotUpdated(ISlot slot)
+        {
+            OnSlotUpdated?.Invoke(slot);
+        }
+
+        public event Action<ResultSlot> OnResultSlotUpdated;
+
+        public void ResultSlotUpdated(ResultSlot slot)
+        {
+            OnResultSlotUpdated?.Invoke(slot);
+        }
+    }
+}
