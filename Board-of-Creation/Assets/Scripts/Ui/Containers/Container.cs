da0a3efd36cc2e3c5cf72e2d3fe1f7f955a4db3c
Alex <alexandruturcanu915@gmail.com>
2020-07-01 18:36:14 +0300

    Added an container for UI interfacing.

---
+++ Board-of-Creation/Assets/Scripts/Ui/Containers/Container.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using BoardOfCreation.Objects;
+using BoardOfCreation.Ui.Slot;
+using UnityEngine;
+
+namespace BoardOfCreation.Ui.Containers
+{
+    public class Container : MonoBehaviour
+    {
+        [SerializeField] protected List<ISlot> slots = new List<ISlot>();
+
+        [SerializeField] protected GameObject slotsPrefab;
+
+        protected void AddSlot(ISlot slot)
+        {
+            var id = slots.Count;
+            slot.Container = this;
+            slot.Id = id;
+            slots.Add(slot);
+        }
+
+        protected void AddSlot(ISlot slot, ItemStack itemStack)
+        {
+            AddSlot(slot);
+            slot.Stack = itemStack;
+        }
+
+        public void Clear()
+        {
+            slots.Clear();
+        }
+
+        public virtual void MoveItemStack(int fromId, int toId)
+        {
+            
+        }
+    }
+}
