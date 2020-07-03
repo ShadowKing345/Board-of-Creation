715d0b8664315ecb9dea2a294b73005b23a12213
Alex <alexandruturcanu915@gmail.com>
2020-07-03 19:50:22 +0300

    Fixed an issue where the size of the layout group was not being adjusted based on the number of objects it has as children.

--- Board-of-Creation/Assets/Scripts/Ui/InventoryGridLayout.cs
+++ Board-of-Creation/Assets/Scripts/Ui/InventoryGridLayout.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace BoardOfCreation.Ui
@@ -13,24 +14,31 @@ namespace BoardOfCreation.Ui
         {
             base.CalculateLayoutInputHorizontal();
 
+            int childrenCount = rectChildren.Count;
             RectOffset padding1;
             cellSize.x = cellSize.y = rectTransform.rect.width / columns - spacing.x / columns * (columns - 1) -
                                       (padding1 = padding).left / (float) columns - padding1.right / (float) columns;
             
-            for (var i = 0; i < rectChildren.Count; i++)
+            for (var i = 0; i < childrenCount; i++)
             {
                 var item = rectChildren[i];
 
                 var xPos = (cellSize.x + spacing.x) * (i % columns) + padding1.left;
-                var yPos = (cellSize.y + spacing.y) * (i / columns) + padding1.left;
+                var yPos = (cellSize.y + spacing.y) * (i / columns) + padding1.top;
                 
                 SetChildAlongAxis(item, 0, xPos, cellSize.x);
                 SetChildAlongAxis(item, 1, yPos, cellSize.y);
             }
+
+            float rectSize = (float) (padding1.top + padding1.bottom + ( cellSize.y + spacing.y ) * Math.Ceiling((float)childrenCount / columns)); 
+            SetLayoutInputForAxis(rectSize, rectSize, 1, 1);
         }
 
         public override void CalculateLayoutInputVertical() { }
+
         public override void SetLayoutHorizontal() { }
+        
+
         public override void SetLayoutVertical() { }
     }
 }
